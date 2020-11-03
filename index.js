ecf01783a702b8d669c6a398bc3a54d6b404dfee
ludwigcarlsson <zhendiac@gmail.com>
2020-11-03 15:02:51 +0100

    fixed comments and textalign

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const card = document.getElementById("card")
+const card = document.getElementById("card") // get the card-element
+
 document.getElementById("get-card").addEventListener("click", function() {
-    card.style.display = "block"
+    setStyle()
+}) // add eventlistener to button
+
+function setStyle() {
+    card.style.display = "flex"
     card.style.backgroundColor = document.getElementById("bg-color").value
     card.style.fontFamily = document.getElementById("font").value
     card.style.fontSize = document.getElementById("font-size").value
-    card.innerText = document.getElementById("msg").value
-})
+    card.innerText = document.getElementById("msg").value // set styling and text value to user inputs
+}
